13e21f5960f5ec87e9669d9a10cc91a179b80e7b
GCH-GCH <gch581490@gmail.com>
2026-05-16 12:03:21 +0200

    Add Clepardia branding and premium loader

---
+++ src/data/assets.ts
@@ -0,0 +1,5 @@
+export const logoMain = '/brand/logo/clepardia-logo-main.svg';
+export const logoMainPng = '/brand/logo/clepardia-logo-main.png';
+export const loaderLogo = logoMain;
+export const heroDesktop = '/images/hero/clepardia-hero.jpg';
+export const heroMobile = '/images/hero/clepardia-hero-mobile.jpg';
